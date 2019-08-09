69e82bd607efa0cadeacba5f5a8c3ae595708de8
positivosencolectivo <positivosencolectivo@gmail.com>
2019-08-09 16:18:37 -0400

    ok

---
+++ js/worldclass.js
@@ -0,0 +1,122 @@
+window.onload = function (){
+  $('#carga').fadeOut();
+  $('body').removeClass('no-scroll')
+
+}
+
+$( document ).ready(function() {
+
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 0)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  var medida = function() {
+    if (screen.width >= 992) {
+        // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("nav-cambio");
+      $(".wc").removeClass("oculto");
+
+    } else {
+      $("#mainNav").removeClass("nav-cambio");
+      $(".wc").addClass("oculto");
+    };
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse); 
+    }
+    
+    
+    
+    else {
+
+        // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top >= 1000) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    };
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+    };
+  };
+
+  medida();
+
+  $('#destinos').on('click', 'a', function(){
+    $('.destinos').removeClass('active');
+    var selected = $(this);
+    selected.toggleClass('active');
+});
+
+$('.call').on('click', function(){
+    $('.full-section').toggleClass('aparece');
+    $('#pagina').toggleClass('noscroll');
+    $('.b-nosotros').trigger('click');
+});
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:3,
+  dots:false,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:4,
+          nav:true,
+          loop:false
+      }
+  }
+})
+})(jQuery); // End of use strict
+
+
